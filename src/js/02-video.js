d91b36ba243c67dff360bb333d2f7300c74ffd72
Bartosz Drzazga <bartosz.drzazga30@gmail.com>
2023-06-02 18:35:07 +0200

    git innit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+player.on('pause', function () {
+  console.log('paused the video!');
+});
+
+function storageTime(data) {
+  const { seconds } = data;
+  console.log(`${seconds}s`);
+  localStorage.setItem('videoplayer-current-time', seconds);
+  return seconds;
+}
+function loadTime() {
+  const lastSavedTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(lastSavedTime);
+}
+
+player.on('timeupdate', throttle(storageTime, 1000));
+window.addEventListener('load', loadTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const emailInput = document.querySelector(`input[name="email"]`);
+const message = document.querySelector(`textarea[name="message"]`);
+const feedbackForm = document.querySelector('.feedback-form');
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const { email: localEmail, message: localMessage } = localData;
+
+  const submitedInfo = {
+    email: localEmail,
+    message: localMessage,
+  };
+
+  console.log(submitedInfo);
+
+  localData.email = '';
+  localData.message = '';
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(localData));
+  feedbackForm.reset();
+}
+
+function saveValue(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = feedbackForm;
+
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+window.addEventListener('load', () => {
+  const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  emailInput.value = localData.email;
+  message.value = localData.message;
+});
+feedbackForm.addEventListener('input', throttle(saveValue, 500));
+feedbackForm.addEventListener('submit', handleSubmit);
+
+// console.log(localStorage.getItem('feedback-form-state'));
